a4e17bd3d8e6fc0aa3a204e969b3d6d7c28dd96b
William Ferreira <williammferreira@gmail.com>
2023-04-16 15:00:55 -0400

    Add command files

---
+++ load_tests.py
@@ -0,0 +1,30 @@
+import sys
+from unittest import TestSuite
+
+from boot_django import boot_django
+
+boot_django()
+
+default_labels = ["safety.tests", ]
+
+
+def get_suite(test_labels=None):
+    from django.test.runner import DiscoverRunner
+
+    if test_labels is None:
+        test_labels = default_labels
+
+    runner = DiscoverRunner(verbosity=1)
+    failures = runner.run_tests(test_labels)
+
+    if failures:
+        sys.exit(failures)
+
+    return TestSuite()
+
+
+if __name__ == "__main__":
+    labels = default_labels
+    if len(sys.argv[1:]) > 0:
+        labels = sys.argv[1:]
+    get_suite(labels)
