96f953d335a33f972d869378e4ffee019926bf9e
William Ferreira <williammferreira@gmail.com>
2023-04-16 15:50:45 -0400

    Add Object Permission model

--- safety/models.py
+++ safety/models.py
@@ -3,35 +3,23 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class ObjectPermissionAbstract(models.Model):
+class ObjectPermission(models.Model):
     """
     Contains the fields that are common to both user and group object permissions.
     """
 
     permission = models.ForeignKey('auth.Permission', on_delete=models.CASCADE, verbose_name=_('Permission'))
 
-    object_pk = models.CharField(_('object ID'), max_length=255)
+    to = GenericForeignKey('target_ct', 'target_id')
+    to_id = models.CharField(_('Target ID'), max_length=255)
+    to_ct = models.ForeignKey('contenttypes.ContentType', on_delete=models.CASCADE, verbose_name=_('Content Type'),
+                              limit_choices_to={'model__in': ('user', 'group')})
+
+    object_id = models.CharField(_('Object ID'), max_length=255)
     object_ct = models.ForeignKey('contenttypes.ContentType', on_delete=models.CASCADE, verbose_name=_('Content Type'))
-    object = GenericForeignKey('object_ct', 'object_pk')
+    object = GenericForeignKey('object_ct', 'object_id')
 
     class Meta:
-        abstract = True
         verbose_name = _('User object permission')
         verbose_name_plural = _('User object permissions')
-        unique_together = (('user', 'permission', 'object_ct', 'object_pk'),)
-
-
-class UserObjectPermission(ObjectPermissionAbstract, models.Model):
-    """
-    Object permissions for users.
-    """
-
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name=_('User'))
-
-
-class GroupObjectPermission(ObjectPermissionAbstract, models.Model):
-    """
-    Object permissions for groups.
-    """
-
-    group = models.ForeignKey('auth.Group', on_delete=models.CASCADE, verbose_name=_('Group'))
+        unique_together = (('to_ct', 'permission', 'object_ct', 'object_pk'),)
