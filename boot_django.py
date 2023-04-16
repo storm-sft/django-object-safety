19a0614e7debeee7de8b105c18815b9f7b62f703
William Ferreira <williammferreira@gmail.com>
2023-04-16 15:30:18 -0400

    Change database backend

--- boot_django.py
+++ boot_django.py
@@ -14,7 +14,7 @@ def boot_django():
         DEBUG=True,
         DATABASES={
             "default": {
-                "ENGINE": "django.db.backends.postgresql",
+                "ENGINE": "django.db.backends.sqlite3",
                 "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
             }
         },
