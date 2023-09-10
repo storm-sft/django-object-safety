1b875b1ce575c9382441600756d22b561e0b9a26
William Ferreira <williammferreira16@gmail.com>
2023-09-10 17:06:29 -0400

    Strip UserRep to basic fields
    Signed-off-by: William Ferreira <williammferreira16@gmail.com>

--- service_perms/utils.py
+++ service_perms/utils.py
@@ -1,14 +1,8 @@
 from dataclasses import dataclass
-from datetime import datetime
 
 
 @dataclass
 class UserRep:
     id: int
     username: str
-    first_name: str
-    last_name: str
     email: str
-    is_staff: bool
-    is_active: bool
-    date_joined: datetime
